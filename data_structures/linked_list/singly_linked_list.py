b0029c432dd6fbd569fa3bc632a698e26a559696
arthur <artuguen28@gmail.com>
2025-10-28 19:15:21 -0400

    feat(linked_list): implement functions to create and detect a cycle in linked lists

--- data_structures/linked_list/singly_linked_list.py
+++ data_structures/linked_list/singly_linked_list.py
@@ -10,10 +10,54 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def convertToCircular(self, pos: int):
+
+        if not self.head:
+            print("The list in empty")
+            return
+        
+        
+        c = 0
+        curr = self.head
+        cycleNode = self.head
+
+        while curr:
+            if c == pos:
+                cycleNode = curr
+
+            if not curr.next:
+                curr.next = cycleNode
+                return
+
+            curr = curr.next
+            c += 1
+
+    def detectCycle(self):
+
+        if not self.head:
+            print("The list in empty")
+            return
+
+
+        fast = self.head
+        slow = self.head
+
+        while fast and fast.next: 
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:  # Cycle detected
+                print("Cycle found!")
+                return True
+
+        print("No cycle found.")
+        return False
+
     def printLinkedList(self):
 
         if not self.head:
             print("The list is empty")
+            return
 
         curr = self.head
         while curr:
@@ -53,7 +97,7 @@ class LinkedList:
         self.head = prev
 
     def deleteNodeByValue(self, value: int):
-        if not self.head:  # Handle empty list
+        if not self.head: 
             print("The list is empty")
             return
 
@@ -78,6 +122,33 @@ class LinkedList:
 
         print("Value not found in the list")
 
+    def deleteNodeByPos(self, pos: int):
+        if not self.head:  # Handle empty list
+            print("The list is empty")
+            return
+
+        if pos == 0:  # Handle deletion of the head node
+            temp = self.head
+            self.head = self.head.next
+            del temp
+            return
+
+        c = 0
+        curr = self.head
+        prev = None
+
+        while curr:
+            if c == pos:
+                prev.next = curr.next
+                del curr
+                return
+            prev = curr
+            curr = curr.next
+            c += 1
+
+        print("Invalid position")  # Position out of bounds
+                
+
 
 if __name__ == "__main__":
 
@@ -87,9 +158,10 @@ if __name__ == "__main__":
     sll.insertNode(6)
     sll.insertNode(10)
     sll.insertNode(17)
-
+    
     sll.printLinkedList()
 
-    sll.reverseList()
+    sll.convertToCircular(1)
+    sll.detectCycle()
 
     sll.printLinkedList()
